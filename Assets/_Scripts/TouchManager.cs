c7a47da0f518dbd512cde6e95bfe717bbd9ebf93
Vitaliy <Skorpion-tracer@yandex.ru>
2023-11-01 00:34:51 +0300

    Логика определения границ камеры

--- Assets/_Scripts/TouchManager.cs
+++ Assets/_Scripts/TouchManager.cs
@@ -1,20 +1,28 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 public class TouchManager : MonoBehaviour
 {
+    [SerializeField] private Transform player;
+
     private PlayerInput playerInput;
 
     private InputAction touchPositionAction;
     private InputAction touchPressAction;
+
+    private Camera mainCamera;
 
     private void OnValidate()
     {
         playerInput ??= GetComponent<PlayerInput>();
         touchPressAction ??= playerInput.actions["TouchPress"];
         touchPositionAction ??= playerInput.actions["TouchPosition"];
+    }
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
     }
 
     private void OnEnable()
@@ -29,7 +37,18 @@ public class TouchManager : MonoBehaviour
 
     private void TouchPressed(InputAction.CallbackContext context)
     {
-        float value = context.ReadValue<float>();
-        Debug.Log(value);
+        Vector3 position = mainCamera.ScreenToWorldPoint(touchPositionAction.ReadValue<Vector2>());
+        position.z = player.transform.position.z;
+        player.transform.position = position;
+    }
+
+    private void Update()
+    {
+        if (touchPressAction.WasPerformedThisFrame())
+        {
+            Vector3 position = mainCamera.ScreenToWorldPoint(touchPositionAction.ReadValue<Vector2>());
+            position.z = player.transform.position.z;
+            player.transform.position = position;
+        }
     }
 }
